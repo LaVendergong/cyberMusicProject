034730d1e7367de8c1ec2f82259f650f8e0535c7
LaVendergong <gong111115@outlook.com>
2025-05-18 15:57:56 +0800

    优化后端代码以适应Vercel Serverless环境

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,6 @@ const apiRouter = require('./routes/api')
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,12 +24,11 @@ app.use(cors({
     'http://127.0.0.1:3000',
     'http://localhost:3000',
     'https://lavendergong.github.io/cyberMusicProject'
-
-  ], // 指定前端域名
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
-// 确保静态文件服务也启用CORS
+
 app.use(express.static(path.join(__dirname, 'public'), {
   setHeaders: (res) => {
       res.set('Access-Control-Allow-Origin', '*');
@@ -46,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'public'), {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api',apiRouter)
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,13 +47,12 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: process.env.NODE_ENV === 'development' ? err : {}
+  });
 });
 
+// Export the Express app
 module.exports = app;
