b18691af279298e0fcef30c6421c0cf6ceabac04
LaVendergong <gong111115@outlook.com>
2025-05-19 14:42:27 +0800

    little change

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 全局配置
 window.AppConfig = {
     // API基础地址
-    API_BASE_URL: 'https://cyber-music-project.vercel.app',
+    API_BASE_URL: 'http://127.0.0.1:3000',
     
     // API端点
     ENDPOINTS: {
