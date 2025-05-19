c572e705691e253b9a786c186ebbfe2799316124
LaVendergong <gong111115@outlook.com>
2025-05-19 19:39:40 +0800

    modified:   ../backend2/src/app.js

--- backend2/src/app.js
+++ backend2/src/app.js
@@ -18,6 +18,12 @@ const healthRoutes = require('./routes/healthRoutes');
 
 const app = express();
 
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    next();
+})
 // 连接数据库
 connectDB();
 
